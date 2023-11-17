2eb70e0159c7be155799270906946e0fe5a276cc
hamza ennaji <persona5magician@gmail.com>
2023-11-17 23:46:05 +0100

    11/17

--- test.js
+++ test.js
@@ -7,22 +7,23 @@ class Graph{
             this.adjacents[node]=new Set();
         }
     }
-    add_link(node1, node2){
+    add_link(node1, node2, weight){
         if(!this.adjacents[node1]){
             this.addlink(node1);
         }
         if(!this.adjacents[node2]){
             this.addlink(node2);
         }
-        this.adjacents[node1].add(node2);
-        this.adjacents[node2].add(node1);
-    }
-    add_weight(node1, node2, weight){
+        this.adjacents[node1].add(node2,weight);
+        this.adjacents[node2].add(node1,weight);
+
 
     }
+
     display(){
         for(let node in this.adjacents){
-            console.log(node+"->"+[...this.adjacents[node]]);
+            console.log(node+"->"+[...this.adjacents[node]]+" "+);
+
         }
     }
 }
@@ -30,11 +31,9 @@ class Graph{
 let graph=new Graph();
 graph.add_node("a");
 graph.add_node("b");
-graph.add_node("c");
-graph.add_node("d");
 
-graph.add_link("a","b")
-graph.add_link("b","c")
-graph.add_link("c","d")
+
+graph.add_link("a","b",1)
+
 
 graph.display();
