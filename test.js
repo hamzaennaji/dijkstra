dcb173f155b1a78e256f3462026aae3d2bb7633f
hamza ennaji <persona5magician@gmail.com>
2023-11-17 23:56:41 +0100

    added link between nodes

--- test.js
+++ test.js
@@ -14,15 +14,13 @@ class Graph{
         if(!this.adjacents[node2]){
             this.addlink(node2);
         }
-        this.adjacents[node1].add(node2,weight);
-        this.adjacents[node2].add(node1,weight);
-
-
+        this.adjacents[node1].add([node2,weight]);
+        this.adjacents[node2].add([node1,weight]);
     }
 
     display(){
         for(let node in this.adjacents){
-            console.log(node+"->"+[...this.adjacents[node]]+" "+);
+            console.log(node+"->"+[...this.adjacents[node]]);
 
         }
     }
@@ -31,9 +29,10 @@ class Graph{
 let graph=new Graph();
 graph.add_node("a");
 graph.add_node("b");
+graph.add_node("c");
+graph.add_node("d");
 
-
-graph.add_link("a","b",1)
-
-
+graph.add_link("a","b",1);
+graph.add_link("b","c",2);
+graph.add_link("c","d",9);
 graph.display();
