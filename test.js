2793de3e65418ce5ebc70be5ed91d76fed2c6ad0
hamza ennaji <persona5magician@gmail.com>
2023-11-18 21:46:41 +0100

    classes complete

--- test.js
+++ test.js
@@ -1,26 +1,30 @@
 class Graph{
     constructor(){
-        this.adjacents={};
+        this.nodes_list={};
     }
     add_node(node){
-        if(!this.adjacents[node]){
-            this.adjacents[node]=new Set();
+        if(!this.nodes_list[node]){
+            this.nodes_list[node]=new Set();
         }
     }
     add_link(node1, node2, weight){
-        if(!this.adjacents[node1]){
-            this.addlink(node1);
+        if(!this.nodes_list[node1]){
+            this.add_node(node1);
         }
-        if(!this.adjacents[node2]){
-            this.addlink(node2);
+        if(!this.nodes_list[node2]){
+            this.add_node(node2);
         }
-        this.adjacents[node1].add([node2,weight]);
-        this.adjacents[node2].add([node1,weight]);
+        this.nodes_list[node1].add([node2,weight]);
+        this.nodes_list[node2].add([node1,weight]);
+    }
+
+    link_exist(node1, node2){
+        return ([...this.nodes_list[node1]][0][0]===node2&&[...this.nodes_list[node2]][0][0]===node1);
     }
 
     display(){
-        for(let node in this.adjacents){
-            console.log(node+"->"+[...this.adjacents[node]]);
+        for(let node in this.nodes_list){
+            console.log(node+"->"+[...this.nodes_list[node]]);
 
         }
     }
@@ -32,7 +36,11 @@ graph.add_node("b");
 graph.add_node("c");
 graph.add_node("d");
 
+
 graph.add_link("a","b",1);
+graph.add_link("b","d",3);
 graph.add_link("b","c",2);
 graph.add_link("c","d",9);
+
 graph.display();
+console.log(graph.link_exist("a", "b"));
