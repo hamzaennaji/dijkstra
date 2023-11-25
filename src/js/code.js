794f55fb56b8d8686b7999295fb02363688280c3
hamza ennaji <persona5magician@gmail.com>
2023-11-25 02:02:51 +0100

    fixed the weight span rotation 🐱‍👤

--- src/js/code.js
+++ src/js/code.js
@@ -1,6 +1,6 @@
 function visualize(g) {
     let list=g.nodeList;
-    // let neighbors=[];d
+    // let neighbors=[];
     function drawevErything(list){
        for(let node in list){
         $("#field").append(//create circles
@@ -16,7 +16,7 @@ function visualize(g) {
                 // neighbors.push({start:node,end:links,id:node+"-"+links});
                 // console.log("links:"+links);
                 $("#field").append($("<span>").addClass("link").attr("id",node+"-"+links));//create line
-                $("#field").append($("<span>").addClass("weight").attr("id","weight").html(list[node][links]));//create weight
+                $("#"+node+"-"+links).append($("<span>").addClass("weight").html(list[node][links]));//create weight
             }
         }
     }
@@ -52,96 +52,27 @@ function visualize(g) {
             console.table(Object.keys(path)[Object.keys(path).indexOf(k)+1]);
         }
     }
-    // for(let node in list){
-    //     $("#"+node).on("click",function(){
-    //         //  console.log($(this).attr('id')+":");console.log($(this).position());
-            
-    //         console.log("nodes:"+node);
-    //         // console.log($(this).attr("id"));
-    //         if(Object.keys(list[node]).length>=1){
-    //             for(let links in list[node]){
-    //                 // console.log(link+":");console.log($("#"+link).position());
-    //                 var a=$("#"+node);
-    //                 var b=$("#"+links);
-    //                 $(".node").draggable({
-    //                     drag:function(event, ui){
-    //                         updateLinks($link,a,b);
-    //                     }
-    //                 });
-    //             }
-    //         }
-    //     });
-    // }
-    // function updateLinks($link,$a,$b){
-    //     var aX = $a.position().left + $a.width() / 2;
-    //     var aY = $a.position().top + $a.height() / 2;
-    //     var bX = $b.position().left + $b.width() / 2;
-    //     var bY = $b.position().top + $b.height() / 2;
-    //     var distance = Math.sqrt(Math.pow(bX - aX, 2) + Math.pow(bY - aY, 2));
-    //     var angle = Math.atan2(bY - aY, bX - aX) * (180 / Math.PI);
-    //     $link.css({
-    //         left: aX,
-    //         top: aY,
-    //         width: distance,
-    //         transform: "rotate(" + angle + "deg)"
-    //       });
-    // }
     function updateLinks($link, $a, $b) {
-        var aX = $a.position().left + $a.width() / 2;
-        var aY = $a.position().top + $a.height() / 2;
-        var bX = $b.position().left + $b.width() / 2;
-        var bY = $b.position().top + $b.height() / 2;
+        let aX = $a.position().left + $a.width() / 2;
+        let aY = $a.position().top + $a.height() / 2;
+        let bX = $b.position().left + $b.width() / 2;
+        let bY = $b.position().top + $b.height() / 2;
         // gpt said : stick to the circle son!
         // dont touch it! it's working!!
-        var midX = (aX + bX) / 2;
-        var midY = (aY + bY) / 2;
-        var distance = Math.sqrt(Math.pow(bX - aX, 2) + Math.pow(bY - aY, 2));
-        var angle = Math.atan2(bY - aY, bX - aX) * (180 / Math.PI);
+        let midX = (aX + bX) / 2;
+        let midY = (aY + bY) / 2;
+        let distance = Math.sqrt(Math.pow(bX - aX, 2) + Math.pow(bY - aY, 2));
+        let angle = Math.atan2(bY - aY, bX - aX) * (180 / Math.PI);
         $link.css({
           left: midX - distance / 2,
           top: midY - $link.height() / 2,
           width: distance,
           transform: "rotate(" + angle + "deg)"
         });
-        $(".weight").css({
-            left: midX - distance / 8,
-            top: midY - $link.height() / 2,
+        $link.find(".weight").css({
+            transform: "rotate(" + (-angle) + "deg)"
         });
         }
-      
-    // $(".node").droppable();
-
-    // for (let node in g.nodeList) {
-    //     let neighbors = g.nodeList[node];
-    //     for (let neighbor in neighbors) {
-    //         // Connect the nodes with spans
-    //         let span = document.createElement('span');
-    //         span.className = 'link';
-    //         span.style.left = (getX(node) + getX(neighbor)) / 2 + 'px';
-    //         span.style.top = (getY(node) + getY(neighbor)) / 2 + 'px';
-    //         span.style.width = getDistance(node, neighbor) + 'px';
-    //         span.innerHTML = neighbors[neighbor];
-    //         document.body.appendChild(span);
-    //     }
-    // }
-
-    // function getX(node) {
-    //     let element = document.getElementById(node);
-    //     return element ? parseFloat(element.style.left) + 20 : 0; // Adjust for circle width
-    // }
-
-    // function getY(node) {
-    //     let element = document.getElementById(node);
-    //     return element ? parseFloat(element.style.top) + 20 : 0; // Adjust for circle height
-    // }
-
-    // function getDistance(node1, node2) {
-    //     let x1 = getX(node1);
-    //     let y1 = getY(node1);
-    //     let x2 = getX(node2);
-    //     let y2 = getY(node2);   
-    //     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-    // }
     drawevErything(list);
     initLinks(list);
     dragNode(list);
